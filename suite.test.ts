d02bccea43f71b8f70206196666d634e5e6646c7
javimsevilla <javier.molina.sevilla@gmail.com>
2020-05-21 22:22:24 +0200

    [BLUE] Separa partes del test

--- suite.test.ts
+++ suite.test.ts
@@ -9,25 +9,37 @@ describe('GameOfLife Testing', () => {
   });
 
   test('Puede crear una celula', function () {
+    // Prepara + Actua
     var result = new Cell();
+
+    // Afirma
     expect(result).toBeDefined();
   });
 
   test('La celula está viva cuando se crea', function () {
+    // Prepara
     var expected = true;
     var cell = new Cell();
+
+    // Actua
     var result = cell.isAlive();
+
+    // Afirma
     expect(result).toBe(expected);
   });
 
   test('La celula muere cuando tiene menos de 2 celulas vecinas vivas', function () {
+    // Prepara
     const NUMBER_OF_NEIGHBORS = 1;
     let expected = false;
     let cell = new Cell();
     let game = new GameOfLife();
+
+    // Actua
     cell.setState(game.getNextState(cell.isAlive(), NUMBER_OF_NEIGHBORS));
     let result = cell.isAlive();
 
+    // Afirma
     expect(result).toBe(expected);
   });
 });
