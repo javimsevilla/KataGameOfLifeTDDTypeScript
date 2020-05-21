4311edc29050f0812a971fe9d969dbbfcf2701f3
javimsevilla <javier.molina.sevilla@gmail.com>
2020-05-21 15:32:45 +0200

    [GREEN] La suite de test funciona correctamente

--- suite.test.ts
+++ suite.test.ts
@@ -3,7 +3,7 @@ import { GameOfLife } from './GameOfLife';
 describe('GameOfLife Testing', () => {
   test('La suite de test funciona correctamente', function () {
     var expected = true;
-    var result = false;
+    var result = true;
     expect(result).toBe(expected);
   });
 });
