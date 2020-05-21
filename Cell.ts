768b2c1fef984c7f064e16f4a86dc5f617725b21
javimsevilla <javier.molina.sevilla@gmail.com>
2020-05-21 22:13:28 +0200

    [BLUE] Extract constant

--- Cell.ts
+++ Cell.ts
@@ -1,5 +1,7 @@
 export class Cell {
-  constructor(private state: boolean = true) {}
+  private static readonly ALIVE_STATE = true;
+
+  constructor(private state: boolean = Cell.ALIVE_STATE) {}
 
   isAlive(): boolean {
     return this.state;
