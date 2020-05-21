5436430b69ecadb72d51c412ab44e48c4b1e7a1d
javimsevilla <javier.molina.sevilla@gmail.com>
2020-05-21 22:27:09 +0200

    [GREEN] La celula se mantiene viva con 3 celulas vecinas vivas

--- GameOfLife.ts
+++ GameOfLife.ts
@@ -2,6 +2,6 @@ export class GameOfLife {
   constructor() {}
 
   getNextState(previousState: boolean, numberOfNeighbors: number): boolean {
-    return previousState && numberOfNeighbors === 2;
+    return previousState && (numberOfNeighbors === 2 || numberOfNeighbors === 3);
   }
 }
